d792fac7cd715bd44882e9a6a6cfa84a9cf8bee0
Tiago Mendes <tetigo@gmail.com>
2021-04-03 01:09:52 -0300

    Alter Components

--- src/index.jsx
+++ src/index.jsx
@@ -6,40 +6,24 @@ export const BookList = () => {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
     </section>
   )
 }
 
 const Book = () => {
+  const author = 'Thiago Nigro'
+  const title = 'Do Mil ao Milhão. Sem Cortar o Cafezinho.'
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/41TsvI70n9L._SX335_BO1,204,203,200_.jpg"
+        alt="livro do mil ao milhão"
+        width="150px"
+      />
+      <h3>{title}</h3>
+      <h4>{author}</h4>
     </article>
   )
 }
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/41TsvI70n9L._SX335_BO1,204,203,200_.jpg"
-      alt="livro do mil ao milhão"
-      width="150px"
-    />
-  )
-}
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-    Thiago Nigro
-  </h4>
-)
-const Title = () => (
-  <h3>
-    Do Mil ao Milhão.
-    <br /> Sem Cortar o Cafezinho.
-  </h3>
-)
 
 ReactDOM.render(<BookList />, document.getElementById('root'))
